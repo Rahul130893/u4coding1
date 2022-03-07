2b886c77471259f1488c8d766b066354dc66b14e
Rahul130893 <93374760+Rahul130893@users.noreply.github.com>
2022-03-07 12:39:52 +0530

    somecorrection

--- index.js
+++ index.js
@@ -5,22 +5,40 @@ const app=express()
 app.use(logger)
 
 app.get("/books", (req, res)=>{
-    res.send({route: "/books"})
+    res.send({route: "/books", permission:req.valid})
 })
 
-app.get("/libraries", (req, res)=>{
-    res.send({route:"/libraries", permission: true})
+
+app.use(checkPermission("req.path"))
+
+
+app.get("/libraries",  (req, res)=>{
+    res.send({route:"/libraries", permission:req.valid})
 })
 
 
 app.get("/authors", (req, res)=>{
-    res.send({route: "/authors", permission:true})
+    res.send({route: "/authors", permission:req.valid})
 })
 
 
 function logger(req, res, next){
     console.log(req.path)
     next()
+}
+
+function checkPermission(x){
+    return function  middle(req, res, next){
+         if(req.path==="/libraries"){
+            req.valid=true;
+             next()
+         } else if(req.path==="/authors"){
+             req.valid=true;
+             next()
+         }
+
+    }
+
 }
 
 app.listen(5000, ()=>{
